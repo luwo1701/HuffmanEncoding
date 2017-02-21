99eb092c3af659de908ee56623f6c431e09e195a
guynameddan <guynameddan@gmail.com>
2017-02-21 15:45:36 -0700

    Update Huffman.py
    Just changed the variable name to 'S' since the HW has it that way.

--- Huffman.py
+++ Huffman.py
@@ -5,8 +5,9 @@ def stringToFreq(stringInput):
     for char in stringInput:
         freqArray[ord(char.lower())-97] = freqArray[ord(char.lower())-97] + 1
         #print freqArray[ord(char.lower())-97]
-        a = dict.fromkeys(stringInput).keys()        
-    return freqArray, sorted(sorted(a), key=str.lower)
+        S = dict.fromkeys(stringInput).keys()   
+        S.sort()
+    return freqArray, S
 
 # print the histogram and unique characters in the string, in lexographical order
 print stringToFreq(inputString)
