ace17b41d6365b19b90d7ba805e9a4fa9843024c
guynameddan <guynameddan@gmail.com>
2017-02-21 16:05:05 -0700

    Update Huffman.py
    started the 'huffmanEncode' function

--- Huffman.py
+++ Huffman.py
@@ -5,9 +5,25 @@ def stringToFreq(stringInput):
     for char in stringInput:
         freqArray[ord(char.lower())-97] = freqArray[ord(char.lower())-97] + 1
         #print freqArray[ord(char.lower())-97]
-        S = dict.fromkeys(stringInput).keys()   
+        S=dict.fromkeys(stringInput).keys()
         S.sort()
     return freqArray, S
 
-# print the histogram and unique characters in the string, in lexographical order
+# print 2 arrays: the histogram and then number of unique 
+# characters in the string, in lexicographical order
 print stringToFreq(inputString)
+
+
+def huffmanEncode(S,f):  
+        H = []
+        n = len(f)
+        for i in range(1,n+1):
+                H.insert(i,f[i])
+        for k in range(n+1,2*n):
+                i = H.deletemin(), j = H.deletemin()
+                create a node numbered k with children i,j
+                f[k] = f[i] + f[j]
+                H.insert(k,f[k])
+
+        create and return codebook T
+
