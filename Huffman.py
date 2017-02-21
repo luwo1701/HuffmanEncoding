acf926256de39d0be7f4c561c69b367061ffb4db
guynameddan <guynameddan@gmail.com>
2017-02-21 15:17:40 -0700

    Update Huffman.py
    Added an array to put the letters in the string in lexographical order. But do not know how to print only the unique ones.

--- Huffman.py
+++ Huffman.py
@@ -1,12 +1,11 @@
 inputString="AAABBBBBBBBCCDEFFFFFFGIIIIJJz"
 freqArray=[0]*26
+unique = []
 def stringToFreq(stringInput):
     for char in stringInput:
         freqArray[ord(char.lower())-97] = freqArray[ord(char.lower())-97] + 1
         #print freqArray[ord(char.lower())-97]
-    return freqArray
+    return freqArray, sorted(sorted(stringInput), key=str.upper)
 
-
+# print the histogram and unique characters in the string, in lexographical order
 print stringToFreq(inputString)
-
-
